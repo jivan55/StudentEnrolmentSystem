e8952104456b4720c4ee9086f1b6b4d1f9b54080
jivan55 <jivanghimire22@gmail.com>
2026-04-19 18:39:05 +1000

    added some comments on the main file

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,36 @@
+// Student Enrolment System
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("S001", "Jeevan", "Jeevan@gmail.com");
-        Student s2 = new Student("S002", "Diwas", "Diwas@gmail.com");
 
-        Course c1 = new Course("CS101", "Programming", 2);
-        Course c2 = new Course("CS201", "Data Structures", 2);
-        c2.addPrerequisite("CS101");
+        // create 2 students
+        Student s1 = new Student("S2400369", "Jeevan", "Jeevan@gmail.com");
+        Student s2 = new Student("S2400370", "Diwas", "Diwas@gmail.com");
+
+        Course c1 = new Course("ITS101", "Programming", 2);
+        Course c2 = new Course("ITS201", "Data Structures", 2);
+        c2.addPrerequisite("ITS101");
 
 
-        System.out.println(" STUDENTS ");
+        System.out.println(" ==>STUDENTS <==");
         s1.displayInfo();
         s2.displayInfo();
 
-        System.out.println("\n COURSES ");
+        System.out.println("\n ==> COURSES <== ");
         c1.displayDetails();
         c2.displayDetails();
 
         ArrayList<Enrolment> enrolments = new ArrayList<>();
         ArrayList<String> completedCourses;
 
-        System.out.println("\n ENROLMENTS ");
+        System.out.println("\n==>ENROLMENT<==");
         LocalDate today = LocalDate.now();
 
         enrolments.add(new Enrolment(s1, c1, today));
         enrolments.add(new Enrolment(s1, c2, today));
-        System.out.println("Jeevan enrolled in CS101 and CS201");
+        System.out.println("Jeevan enrolled in ITS101 and ITS201");
 
         completedCourses = new ArrayList<>();
         for (Enrolment e : enrolments) {
@@ -36,19 +39,21 @@ public class Main {
             }
         }
 
-        if (!completedCourses.contains("CS101")) {
-            System.out.println("Diwas cannot enrol in CS201 - missing CS101 prerequisite");
+        // prerequisite checking (even student id)
+        if (!completedCourses.contains("ITS101")) {
+            System.out.println("Diwas cannot enrol in ITS201 - missing ITS101 prerequisite");
         } else {
             enrolments.add(new Enrolment(s2, c2, today));
-            System.out.println("Diwas enrolled in CS201");
+            System.out.println("Diwas enrolled in ITS201");
         }
-
+        // enrolling diwas
         enrolments.add(new Enrolment(s2, c1, today));
-        System.out.println("Diwas enrolled in CS101");
+        System.out.println("Diwas enrolled in ITS101 and now can enrolled in another unit");
 
         enrolments.add(new Enrolment(s2, c2, today));
-        System.out.println("Diwas enrolled in CS201 (now has prerequisite)");
+        System.out.println("Diwas enrolled in ITS201");
 
+        //display all enrolments with dates
         System.out.println("\n ALL ENROLMENTS ");
         for (Enrolment e : enrolments) {
             e.displayDetails();

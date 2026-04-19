165614883ce32b0ea9cfdb87b6c51c64ba46b7a5
jivan55 <jivanghimire22@gmail.com>
2026-04-19 14:47:49 +1000

    added comments to student class

--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,22 @@
+// represent a student in the enrolment system
 public class Student {
+    // private attributes for encapsulation
     private String id;
     private String name;
     private String email;
 
+    //constructor
     public Student(String id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
+    //getter methods
     public String getName() { return name; }
     public String getId() { return id; }
 
+    // display student information
     public void displayInfo() {
         System.out.println(id + " | " + name + " | " + email);
     }

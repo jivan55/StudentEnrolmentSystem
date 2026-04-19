42005f3d52bce5fdc4a9ecff0f9ac175fe3c3064
jivan55 <jivanghimire22@gmail.com>
2026-04-19 14:50:51 +1000

    added comments to course class

--- src/Course.java
+++ src/Course.java
@@ -1,3 +1,4 @@
+// represent a course with prerequisites
 import java.util.ArrayList;
 
 public class Course {
@@ -6,6 +7,7 @@ public class Course {
     private int maxCapacity;
     private ArrayList<String> prerequisites = new ArrayList<>();
 
+    //constructor
     public Course(String code, String name, int maxCapacity) {
         this.code = code;
         this.name = name;
@@ -16,10 +18,11 @@ public class Course {
     public int getMaxCapacity() { return maxCapacity; }
     public ArrayList<String> getPrerequisites() { return prerequisites; }
 
+    // add a prerequisites course
     public void addPrerequisite(String prereq) {
         prerequisites.add(prereq);
     }
-
+    // display course details
     public void displayDetails() {
         System.out.println(code + " - " + name + " (Max: " + maxCapacity + ")");
         System.out.println("  Prerequisites: " + prerequisites);
